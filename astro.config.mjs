1e6fe20640d2d51451559dea2f805b15a1767a62
mnokerdev <mnoker.dev@gmail.com>
2025-06-13 00:37:51 -0700

    refactor(api): added import for env variables

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 // @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig, envField } from "astro/config";
 
 import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
@@ -11,4 +11,11 @@ export default defineConfig({
     plugins: [tailwindcss()],
   },
   output: "server",
+  env: {
+    schema: {
+      RESEND_API_KEY: envField.string({ context: "server", access: "secret" }),
+      SENT_FROM_EMAIL: envField.string({ context: "server", access: "secret" }),
+      SEND_TO_EMAIL: envField.string({ context: "server", access: "secret" }),
+    },
+  },
 });
