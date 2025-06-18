3d3ee86433ec4a22bdc6160d42d714372444b6fc
mnokerdev <mnoker.dev@gmail.com>
2025-06-18 16:04:04 -0700

    test(page): add test page to test button styles chore(deps): add clsx for class control style(global): changed light mode border color

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,8 +1,14 @@
 import type { ReactNode } from "react";
+import clsx from "clsx";
+
+type Variant = "default" | "secondary" | "primary" | "destructive" | "ghost";
+type Size = "default" | "sm" | "lg" | "icon";
+type Rounded = "default" | "sm" | "lg" | "none";
 
 type Props = React.ComponentProps<"button"> & {
-  variant?: "default" | "secondary" | "primary" | "destructive" | "ghost";
-  size?: "default" | "sm" | "lg" | "icon";
+  variant?: Variant;
+  size?: Size;
+  rounded?: Rounded;
   children: ReactNode;
 };
 
@@ -11,49 +17,43 @@ function Button({
   className,
   variant = "default",
   size = "default",
+  rounded = "default",
   ...props
 }: Props) {
-  let colors = "";
-  let sizes = "";
-  switch (variant) {
-    case "default":
-      colors = "bg-foreground text-background hover:bg-foreground/80";
-      break;
-    case "secondary":
-      colors = "bg-secondary text-secondary-foreground hover:bg-secondary/80";
-      break;
-    case "primary":
-      colors = "bg-primary text-primary-foreground hover:bg-primary/80";
-      break;
-    case "destructive":
-      colors =
-        "bg-destructive text-secondary dark:text-secondary-foreground hover:bg-destructive/80";
-      break;
-    case "ghost":
-      colors = "border-none hover:scale-110";
-      break;
-    default:
-      colors = "";
-  }
-  switch (size) {
-    case "default":
-      sizes = "h-10 px-3";
-      break;
-    case "sm":
-      sizes = "h-9 px-3";
-      break;
-    case "lg":
-      sizes = "h-11 px-4";
-      break;
-    case "icon":
-      sizes = "h-9 w-9 p-1";
-      break;
-    default:
-      sizes = "";
-  }
+  const base = `inline-flex justify-center border items-center text-sm font-medium tracking-wide gap-2 whitespace-nowrap transition-all duration-200 cursor-pointer disabled:opacity-50 disabled:pointer-events-none disabled:cursor-not-allowed disabled:selection`;
+
+  const variantClasses: Record<Variant, string> = {
+    default: "bg-foreground text-background hover:bg-foreground/80",
+    secondary: "bg-secondary text-secondary-foreground hover:bg-secondary/80",
+    primary: "bg-primary text-primary-foreground hover:bg-primary/80",
+    destructive:
+      "bg-destructive text-secondary dark:text-secondary-foreground hover:bg-destructive/80",
+    ghost: "border border-border bg-foreground/10",
+  };
+
+  const sizeClasses: Record<Size, string> = {
+    default: "h-10 px-3",
+    sm: "h-9 px-3",
+    lg: "h-13 px-6",
+    icon: "h-9 w-9 p-1",
+  };
+
+  const roundedClasses: Record<Rounded, string> = {
+    default: "rounded",
+    sm: "rounded-xs",
+    lg: "rounded-xl",
+    none: "",
+  };
+
   return (
     <button
-      className={`inline-flex justify-center border items-center text-sm font-medium tracking-wide gap-2 whitespace-nowrap rounded ${colors} ${sizes} transition-all duration-200 cursor-pointer disabled:opacity-50 disabled:pointer-events-none disabled:cursor-not-allowed disabled:selection: ${className}`}
+      className={clsx(
+        base,
+        variantClasses[variant],
+        sizeClasses[size],
+        roundedClasses[rounded],
+        className,
+      )}
       {...props}
     >
       {children}
