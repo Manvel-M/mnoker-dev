803ac7701f3dac9e3f8056b16c08cdf1ed91c036
mnokerdev <mnoker.dev@gmail.com>
2025-06-25 13:24:31 -0700

    chore(deps): add cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 // @ts-check
 import { defineConfig, envField } from "astro/config";
+import cloudflare from "@astrojs/cloudflare";
 
 import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  adapter: cloudflare(),
   integrations: [react()],
   vite: {
     plugins: [tailwindcss()],
