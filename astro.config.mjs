e6ef82ef3fa5cad66736e7c4bf93cfa16a820997
mnokerdev <mnoker.dev@gmail.com>
2025-06-25 20:25:17 -0700

    fix(config): add workaround to prod cloudflare error

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,25 @@
 // @ts-check
-import { defineConfig, envField } from "astro/config";
+import { defineConfig } from "astro/config";
 import cloudflare from "@astrojs/cloudflare";
-
 import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "server",
   adapter: cloudflare({
     imageService: "cloudflare",
   }),
   integrations: [react()],
   vite: {
     plugins: [tailwindcss()],
+    resolve: {
+      alias:
+        process.env.NODE_ENV === "production"
+          ? {
+              "react-dom/server": "react-dom/server.edge",
+            }
+          : undefined,
+    },
   },
-  output: "server",
 });
